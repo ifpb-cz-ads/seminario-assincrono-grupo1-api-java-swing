740b13316037f99fe9012a1fd2264a52d3ef9c76
camila-casimiro <camila.casimiro@academico.ifpb.edu.br>
2020-12-13 20:24:09 -0200

    Adicionando novas funcionalidades no código do cadastro

--- Código cadastro/Main.java
+++ Código cadastro/Main.java
@@ -3,71 +3,96 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class Main implements ActionListener  {
+public class Main implements ActionListener {
 
-    private JButton button = new JButton("Enviar");
-    private JFrame frame = new JFrame();
-    private JLabel emailLabel = new JLabel("Digite seu email");
-    private JTextField inputEmail = new JTextField();
-    private JTextField inputPassword = new JTextField();
-    private JLabel passwordLabel = new JLabel("Digite sua senha");
+	private JButton button = new JButton("Enviar");
+	private JFrame frame = new JFrame("Cadastro");
+	private JLabel nomeLabel = new JLabel("Digite seu nome");
+	private JTextField inputNome = new JTextField();
+	private JLabel emailLabel = new JLabel("Digite seu email");
+	private JTextField inputEmail = new JTextField();
+	private JPasswordField inputPassword = new JPasswordField();
+	private JLabel passwordLabel = new JLabel("Digite sua senha");
+	private JLabel generoLabel = new JLabel("Genero:");
+	private JRadioButton feminino = new JRadioButton("Feminino");
+	private JRadioButton masculino = new JRadioButton("Masculino");
+	
+	ButtonGroup select=new ButtonGroup();
 
-    @Override
-    public void actionPerformed(ActionEvent evento) {
+	@Override
+	public void actionPerformed(ActionEvent evento) {
 
-        button.setText("Carregando...");
+		button.setText("Carregando...");
 
-        this.openDialog("Estamos processando suas informações...");
-        
-    }
+		this.openDialog("Estamos processando suas informações...");
 
-    public void openDialog(String text){
+	}
 
-        System.out.print(text);
+	public void openDialog(String text) {
 
-        JDialog dialog = new JDialog(frame, "Resposta", true);
-        dialog.setLayout( new FlowLayout() );
+		System.out.print(text);
 
-        JLabel label = new JLabel (text);
-        label.setBounds(150, 50, 100, 40);
-        dialog.add(label);
-         
-        dialog.setSize(300,300);    
-        dialog.setVisible(true);
+		JDialog dialog = new JDialog(frame, "Resposta", true);
+		dialog.setLayout(new FlowLayout());
 
-    }
-    
-    public void janela(){
+		JLabel label = new JLabel(text);
+		label.setBounds(150, 50, 100, 40);
+		dialog.add(label);
 
-        emailLabel.setBounds(50, 50, 100, 40);
-        frame.add(emailLabel);
+		dialog.setSize(300, 300);
+		dialog.setVisible(true);
 
-        inputEmail.setBounds(50, 100, 500, 40);
-        frame.add(inputEmail);
+	}
 
-        passwordLabel.setBounds(50, 150, 100, 40);
-        frame.add(passwordLabel);
+	public void janela() {
 
-        inputPassword.setBounds(50, 200, 500, 40);
-        frame.add(inputPassword);
+		nomeLabel.setBounds(50, 50, 100, 40);
+		frame.add(nomeLabel);
 
-        button.setBounds(250, 300, 100, 40);
-        frame.add(button);
+		inputNome.setBounds(50, 100, 500, 40);
+		frame.add(inputNome);
 
-        button.addActionListener(this);
+		emailLabel.setBounds(50, 150, 100, 40);
+		frame.add(emailLabel);
 
-        frame.setSize(600, 600);
-        frame.setLocation(400, 50);
-        frame.setLayout(null);
-        frame.setVisible(true);
+		inputEmail.setBounds(50, 200, 500, 40);
+		frame.add(inputEmail);
 
-    }
+		passwordLabel.setBounds(50, 250, 100, 40);
+		frame.add(passwordLabel);
 
-    public static void main(String[] args){
+		inputPassword.setBounds(50, 300, 500, 40);
+		frame.add(inputPassword);
 
-        Main controller = new Main();
-        controller.janela();
+		generoLabel.setBounds(50, 350, 500, 40);
+		frame.add(generoLabel);
 
-    }
+		feminino.setBounds(50, 380, 500, 40);
+		frame.add(feminino);
+
+		masculino.setBounds(50, 420, 500, 30);
+		frame.add(masculino);
+
+		select.add(feminino);
+		select.add(masculino);
+
+		button.setBounds(250, 300, 100, 40);
+		frame.add(button);
+
+		button.addActionListener(this);
+
+		frame.setSize(600, 600);
+		frame.setLocation(400, 50);
+		frame.setLayout(null);
+		frame.setVisible(true);
+
+	}
+
+	public static void main(String[] args) {
+
+		Main controller = new Main();
+		controller.janela();
+
+	}
 
 }
